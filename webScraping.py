8203d3b69582944cc69d775a31e57d24f601d568
johnchem <j.payssan@gmail.com>
2021-12-19 15:50:44 +0100

    collect item rating, picture url

--- webScraping.py
+++ webScraping.py
@@ -5,7 +5,8 @@ import bs4
 import os
 import re
 from pathlib import *
-
+import urllib
+#import webbrowser
 
 WEBSITE = "http://books.toscrape.com/"
 TRAVEL_PAGE = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
@@ -20,21 +21,45 @@ def get_category_data(category_url):
 	r = requests.get(category_url)
 	print(r.json)
 
-def get_article_data(article_url):
+def get_article_data(article_url, category = None):
+	all_rating = ["one", "Two", "Three", "Four", "Five"]
+	# initialize the item's data dictionnary
+	item_data = {"product_page_url" : None,
+				"universal_product_code(upc)" : None,
+				"title" : None,
+				"price_including_tax" : None,
+				"price_excluding_tax" : None,
+				"number_available": None,
+				"product_description" : None,
+				"category" : None,
+				"review_rating" : None,
+				"image_url" : None}
 	# collect the raw html
 	r = requests.get(article_url)
 	soup = bs4.BeautifulSoup(r.text, 'html.parser')
+
+	# record the category
+	if category : item_data["category"] = category
 	
+	# get the produt page url
+	item_data["product_page_url"] = article_url
+
 	# get the title
 	result_title = soup.find("div", class_="content").find("h1")
-	print(result_title.get_text())
+	item_data["title"] = result_title.get_text()
 
-	# get the rating
-	re_rating = re.compile("star_rating {*}")
-	result = soup.find_all("a", class_=re_rating)
-	print(result.attrs)
+	# get the rating then turn word into numeric value
+	re_rating = re.compile(r"(?:star-rating) (\w*)")
+	result_rating = soup.find("p", class_=re_rating)
+	raw_rating = result_rating["class"][1]
+	item_data["review_rating"] = all_rating.index(raw_rating)+1
 
+	# get the picture url
+	result_picture = soup.find("img")
+	relative_url = result_picture.attrs["src"]
+	item_data["image_url"] = urllib.parse.urljoin(article_url, relative_url)
 
+	pass
 
 def get_picture():
 	pass
